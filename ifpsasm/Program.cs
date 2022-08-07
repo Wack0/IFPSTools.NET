0ea1a2269f34be3df288bf2db07a14cb3a7f2884
zc <2650838+Wack0@users.noreply.github.com>
2022-08-07 15:51:58 +0100

    ifpsasm: Add some usage and confirmation text.

--- ifpsasm/Program.cs
+++ ifpsasm/Program.cs
@@ -11,11 +11,20 @@ namespace ifpsasm
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: {0} <script.asm>", Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location));
+                Console.WriteLine();
+                Console.WriteLine("RemObjects PascalScript assembler.");
+                Console.WriteLine("Writes the output to the *.bin file in the same directory as the passed in assembly file.");
+                return;
+            }
             var arg = args[0];
             var ext = Path.GetExtension(arg);
             var bin = arg.Substring(0, arg.Length - ext.Length) + ".bin";
             var script = IFPSAsmLib.Assembler.Assemble(File.ReadAllText(args[0]));
             script.Save(File.OpenWrite(bin));
+            Console.WriteLine("Assembled script to {0}", bin);
         }
     }
 }
